42aa6f36a5aa93d5ef9193fa17e72caf6d8e99eb
Anxo Rodriguez <anxolin@gmail.com>
2020-04-22 12:29:07 +0200

    Sort tokens

--- src/SelectToken.js
+++ src/SelectToken.js
@@ -18,15 +18,30 @@ export function SelectToken({ onTokenSelected, value }) {
   if (loading) return "Loading tokens...";
   if (error) return `Error! ${error.message}`;
 
+  const tokens = data ? data.tokens.sort(_sortTokens) : [];
+
   return (
     <select name="token" onChange={onTokenSelected} value={value}>
-      {data.tokens.map(token => (
+      {tokens.map(token => (
         <option key={token.id} value={token.id}>
           {token.symbol || token.address}
         </option>
       ))}
     </select>
   );
+}
+
+function _sortTokens(token1, token2) {
+  const value1 = (token1.symbol || token1.address).toUpperCase();
+  const value2 = (token2.symbol || token2.address).toUpperCase();
+
+  let comparison = 0;
+  if (value1 > value2) {
+    comparison = 1;
+  } else if (value1 < value2) {
+    comparison = -1;
+  }
+  return comparison;
 }
 
 export default SelectToken;
